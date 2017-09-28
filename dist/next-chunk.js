fd29112bbf20de6b78ecb58037841ae08f033867
afeiship <1290657123@qq.com>
2017-09-28 15:54:16 +0800

    optimize src

--- dist/next-chunk.js
+++ dist/next-chunk.js
@@ -2,27 +2,22 @@
 
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
-  var ARRAY_PROTO = Array.prototype;
 
   nx.chunk = function (inArray, inOffset) {
-    var maxSize = Math.ceil(inArray.length / inOffset);
-    var sliceList = [],
-      chunkList = [];
+    var size = Math.ceil(inArray.length / inOffset);
+    var list = [];
 
     //generate chunkList:
-    for (var index = 0; index < maxSize; index++) {
-      sliceList.push([
+    for (var index = 0; index < size; index++) {
+      list.push([
         index * inOffset,
         (index + 1) * inOffset
       ]);
     }
 
-    sliceList
-      .forEach(function (item) {
-        chunkList.push(inArray.slice.apply(inArray, item))
-      });
-
-    return chunkList;
+    return list.map(function (item) {
+      return inArray.slice.apply(inArray, item);
+    });
   };
 
   if (typeof module !== 'undefined' && module.exports) {
