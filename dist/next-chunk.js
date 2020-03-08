5efccb2c62ecd3ca3047e164494a029e423723af
afeiship <1290657123@qq.com>
2020-03-09 01:07:43 +0800

    chore: update build script

--- dist/next-chunk.js
+++ dist/next-chunk.js
@@ -3,7 +3,7 @@
  * description: Creates an array of elements split into groups the length of size.
  * url: https://github.com/afeiship/next-chunk
  * version: 1.0.0
- * date: 2019-12-17 00:00:38
+ * date: 2020-03-09 01:07:27
  * license: MIT
  */
 
@@ -11,8 +11,18 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
 
-  nx.chunk = function() {
-    //code goes here.
+  nx.chunk = function(inArray, inChunk) {
+    var size = Math.ceil(inArray.length / inChunk);
+    var list = [];
+
+    //generate chunkList:
+    for (var index = 0; index < size; index++) {
+      list.push([index * inChunk, (index + 1) * inChunk]);
+    }
+
+    return list.map(function(item) {
+      return inArray.slice.apply(inArray, item);
+    });
   };
 
   if (typeof module !== 'undefined' && module.exports) {
